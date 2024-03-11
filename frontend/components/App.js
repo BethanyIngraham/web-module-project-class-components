d39ffee5bee3916ca7eaa495c4f646d015dd3ca4
Bethany Ingraham <BethFIngraham@gmail.com>
2024-03-11 19:05:53 -0400

    s9 m1 proj in progress first commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Form from './Form';
+import TodoList from './TodoList';
+
+
+const todoTasks = [
+  {name: 'Take out the trash', id: 1234567, completed: false},
+  {name: 'Wash the dishes', id: 9287635, completed: false},
+  {name: 'Laundry', id: 1673542, completed: false},
+  {name: 'Water the lawn', id: 8854276, completed: false},
+  {name: 'Bring back Library book', id: 2754983, completed: false}
+]
 
 export default class App extends React.Component {
+
+  state = {
+    todos: todoTasks
+  };
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos}/>
+        <Form />
       </div>
     )
   }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo';
 
 export default class TodoList extends React.Component {
+  
   render() {
+
+    const {todos} = this.props;
+
     return (
       <div>
-        TodoList
+        Todos: 
+        {todos.map((todo) => {
+          <Todo />
+        })}
       </div>
     )
   }
